0859a7e61def21a64511b3112b2c4167bce50816
seo-ji-hwan <103182652+seo-ji-hwan@users.noreply.github.com>
2022-04-22 13:46:22 +0900

    Delete FifteenPuzzleMain.cpp

--- FifteenPuzzleMain.cpp
+++
@@ -1,9 +0,0 @@
-#include "Ranking.h"
-extern int playFifteenPuzzle();
-int main()
-{
-	loadRanking("ranking.txt");
-	int rank = playFifteenPuzzle();
-	printRanking();
-	storeRanking("ranking.txt");
-}
