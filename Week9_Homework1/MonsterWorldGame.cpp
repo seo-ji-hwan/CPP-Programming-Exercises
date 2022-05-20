19029b45ef5a7c778512bb1a6bfff6cd7272c793
seo-ji-hwan <103182652+seo-ji-hwan@users.noreply.github.com>
2022-05-20 21:33:26 +0900

    Add files via upload

---
+++ Week9_Homework1/MonsterWorldGame.cpp
@@ -0,0 +1,18 @@
+#include "MonsterWorld.h"
+#include "VariousMonsters.h"
+#include <time.h>
+
+void main()
+{
+	srand((unsigned int)time(NULL));
+	int w = 16, h = 8;
+	MonsterWorld game(w, h);
+
+	game.add(new Zombie("허접한좀비", "◎", rand() % w, rand() % h));
+	game.add(new Vampire("뱀파이어짱", "★", rand() % w, rand() % h));
+	game.add(new KGhost("어쩌다귀신", "♥", rand() % w, rand() % h));
+	game.add(new Jiangshi("못먹어도고", "↔", rand() % w, rand() % h, true));
+	game.add(new Jiangshi("못먹어세로", "↕", rand() % w, rand() % h, false));
+	game.play(500, 10);
+	printf("------게임 종료-------------------\n");
+}
